c628be151ddb6c2ddff26282628ac61bc44ee2e3
Rabby2205 <rabbyisalm1626@gmail.com>
2022-02-10 22:20:02 +0600

    link page

--- login.js
+++ login.js
@@ -1,3 +1,17 @@
 document.getElementById('login-button').addEventListener('click', function () {
-    console.log('you are Right')
+    //    catch user gmail
+    const gamilField = document.getElementById('user-gmail');
+    const userGmail = gamilField.value;
+    //  catch user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // condition decleration
+    if (userGmail == 'trail@gmail.com' && userPassword == 'accessOn') {
+
+        window.location.href = "banking.html"
+    }
+    else {
+        window.location.href = "wrong information.html"
+    }
+
 })
