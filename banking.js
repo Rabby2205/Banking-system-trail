fac2d73aceea2ecc7fc8c6a10300eaf66ef49070
Rabby2205 <rabbyisalm1626@gmail.com>
2022-02-11 13:42:45 +0600

    deposit-calculate

--- banking.js
+++ banking.js
@@ -1,8 +1,17 @@
 // handle deposit button event .
 document.getElementById('deposit-button').addEventListener('click', function () {
-    const depositDisplay = document.getElementById('deposit-amount');
-    const depositAmount = depositDisplay.value;
-    depositDisplay.value = '';
+    // get ammount from user
+    const depositField = document.getElementById('deposit-amount');
+    const depositAmountText = depositField.value;
+    const depositAmount = parseFloat(depositAmountText);
+    depositField.value = '';
+    //get deposit
+    const depositDisplay = document.getElementById('deposit-display');
+    const initialDepositDisplayText = depositDisplay.innerText;
+    const initialDepositDisplay = parseFloat(initialDepositDisplayText);
+    const newDepostiAmount = (initialDepositDisplay + depositAmount);
+    depositDisplay.innerText = newDepostiAmount;
+
 
 
 
