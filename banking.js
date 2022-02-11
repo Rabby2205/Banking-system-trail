08eb243096ac65c965036742c07df6c24cc09088
Rabby2205 <rabbyisalm1626@gmail.com>
2022-02-11 14:44:27 +0600

    Complete Calculate

--- banking.js
+++ banking.js
@@ -1,6 +1,6 @@
 // handle deposit button event .
 document.getElementById('deposit-button').addEventListener('click', function () {
-    // get ammount from user
+    // get deposit ammount from user
     const depositField = document.getElementById('deposit-amount');
     const depositAmountText = depositField.value;
     const depositAmount = parseFloat(depositAmountText);
@@ -11,10 +11,32 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     const initialDepositDisplay = parseFloat(initialDepositDisplayText);
     const newDepostiAmount = (initialDepositDisplay + depositAmount);
     depositDisplay.innerText = newDepostiAmount;
-    // calculate balance also
+    // calculate main balance 
     const balanceField = document.getElementById('balance-display');
     const balanceAmountText = balanceField.innerText;
     const balanceAmount = parseFloat(balanceAmountText);
     const totalBalance = balanceAmount + depositAmount;
     balanceField.innerText = totalBalance;
+
+})
+// handle withdraw button event
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    // get withdraw amount from user
+    const withdrawField = document.getElementById('withdraw-amount');
+    const withdrawAmountText = withdrawField.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+    withdrawField.value = '';
+    // withdraw calculate
+    const withdrawDisplay = document.getElementById('withdraw-display');
+    const withdrawDisplayText = withdrawDisplay.innerText;
+    const withdrawAmountDisplay = parseFloat(withdrawDisplayText);
+    const totalWithdrawAmount = withdrawAmountDisplay + withdrawAmount;
+    withdrawDisplay.innerText = totalWithdrawAmount;
+    // reduce balance for withdraw
+    const balanceField = document.getElementById('balance-display');
+    const balanceAmountText = balanceField.innerText;
+    const balanceAmount = parseFloat(balanceAmountText);
+    const totalBalance = balanceAmount - withdrawAmount;
+    balanceField.innerText = totalBalance;
+
 })
